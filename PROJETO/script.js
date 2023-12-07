4b9516872c170c5c05c3eb7b917cb3171ef7f1a0
Hilda Miranda <hildamiranda8@outlook.com>
2023-12-07 15:43:28 -0300

    ajustes

--- PROJETO/script.js
+++ PROJETO/script.js
@@ -67,6 +67,21 @@ document.addEventListener('DOMContentLoaded', function () {
         openLightbox('cover-image.jpg');
     });
 
+    // Adicione um event listener para o formulário
+    document.getElementById('registrationForm').addEventListener('submit', function (event) {
+        event.preventDefault(); // Evita o envio padrão do formulário
+
+        // Obtenha os valores do formulário
+        const name = document.getElementById('name').value;
+        const email = document.getElementById('email').value;
+        const password = document.getElementById('password').value;
+        const phone = document.getElementById('phone').value;
+        const gender = document.getElementById('gender').value;
+
+        // Chame a função saveUser
+        saveUser(name, email, password, phone, gender);
+    });
+
     function openLightbox(imageSrc) {
         const lightboxModal = document.querySelector('.lightbox-modal');
         const lightboxContent = document.querySelector('.lightbox-content img');
@@ -86,8 +101,6 @@ document.addEventListener('DOMContentLoaded', function () {
     }
 
     function navigate(direction) {
-    
-
         currentIndex += direction;
 
         if (currentIndex < 0) {
@@ -99,103 +112,11 @@ document.addEventListener('DOMContentLoaded', function () {
         const imageSrc = images[currentIndex];
         openLightbox(imageSrc);
     }
+    });
 
-    function login() {
-        const storedUser = localStorage.getItem('user');
-        if (storedUser) {
-            const user = JSON.parse(storedUser);
-            const emailInput = document.getElementById('email').value;
-            const passwordInput = document.getElementById('password').value;
-    
-            if (emailInput === user.email && passwordInput === user.password) {
-                showMessage('Login bem-sucedido!');
-                // Aqui você pode redirecionar o usuário para a página de perfil, por exemplo
-            } else {
-                showMessage('Credenciais inválidas. Tente novamente.');
-            }
-        } else {
-            showMessage('Usuário não cadastrado. Faça o cadastro primeiro.');
-        }
-    
-        // Retorna false para evitar o envio real do formulário
-        return false;
-    }
-    
-    function saveUser(name, email, password) {
-        const user = {
-            name: name,
-            email: email,
-            password: password
-        };
-        localStorage.setItem('user', JSON.stringify(user));
-    }
-    
-    function showMessage(message) {
-        // Cria um elemento de mensagem
-        var messageElement = document.createElement('div');
-        messageElement.className = 'message';
-        messageElement.innerHTML = message;
-    
-        // Adiciona a mensagem ao contêiner de mensagens
-        document.getElementById('message-container').appendChild(messageElement);
-    
-        // Remove a mensagem após alguns segundos
-        setTimeout(function () {
-            messageElement.remove();
-        }, 5000);
-    }
-    
 
-    function saveUser(name, email, password) {
-        const storedUsers = JSON.parse(localStorage.getItem('users')) || [];
-        const newUser = { name, email, password };
 
-        // Verifica se o usuário já existe
-        const existingUser = storedUsers.find(user => user.email === email);
-        if (existingUser) {
-            alert('Este e-mail já está cadastrado. Faça login ou use outro e-mail.');
-            return false;
-        }
 
-        storedUsers.push(newUser);
-        localStorage.setItem('users', JSON.stringify(storedUsers));
-        showMessage('Cadastro realizado com sucesso!');
-        return false; // Evita o envio real do formulário
-    }
-
-    function formatPhoneNumber(input) {
-        // Remove caracteres não numéricos
-        let phoneNumber = input.value.replace(/\D/g, '');
-
-        // Aplica a formatação (##) ####-####
-        if (phoneNumber.length === 11) {
-            phoneNumber = phoneNumber.replace(/(\d{2})(\d{5})(\d{4})/, '($1) $2-$3');
-            input.value = phoneNumber;
-        }
-    }
-
-    function validateForm() {
-        // Limpar campos de texto
-        document.getElementById("registrationForm").reset();
-        // Retorna false para evitar o envio real do formulário
-        return false;
-    }
-
-    function showMessage(message) {
-        // Cria um elemento de mensagem
-        var messageElement = document.createElement("div");
-        messageElement.className = "success-message";
-        messageElement.innerHTML = message;
-
-        // Adiciona a mensagem ao contêiner de mensagens
-        document.getElementById("message-container").appendChild(messageElement);
-
-        // Remove a mensagem após alguns segundos
-        setTimeout(function () {
-            messageElement.remove();
-        }, 5000);
-    }
-});
 
     
     
